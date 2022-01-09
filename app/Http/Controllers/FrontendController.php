62c2b13ea21ad6aafa3cf7fc8c590a7fcfe27235
amitoz deol <amitozdeol@amitozs-MacBook-Pro.local>
2022-01-09 16:56:56 -0500

    inital commit

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use App\Models\Forum;
+use App\Models\Discussion;
+use App\Models\User;
+
+
+class FrontendController extends Controller
+{
+    public function index()
+    {
+        $user = new User;
+        $users_online = $user->allOnline();
+        $forumsCount = count(Forum::all());
+        $topicsCount = count(Discussion::all());
+        $totalMembers = count(User::all());
+        $newest = User::latest()->first();
+        $totalCategories = count(Category::all());
+        $categories = Category::latest()->get();
+        return view('welcome', \compact('categories', 'forumsCount', 'topicsCount', 'newest', 'totalMembers', 'totalCategories', 'users_online'));
+    }
+
+    public function categoryOverview($id)
+    {
+        $user = new User;
+        $users_online = $user->allOnline();
+        $forumsCount = count(Forum::all());
+        $topicsCount = count(Discussion::all());
+        $totalMembers = count(User::all());
+        $newest = User::latest()->first();
+        $totalCategories = count(Category::all());
+        $category = Category::find($id);
+
+        return view('client.category-overview', \compact('category'));
+    }
+
+    public function forumOverview($id)
+    {
+        $forum = Forum::find($id);
+        return view('client.forum-overview', \compact('forum'));
+    }
+}
